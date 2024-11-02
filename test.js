1a3481e0b2161460b5f526fb0750fb1e3f694ba3
= <=>
2024-11-02 15:58:02 +0700

    update

--- test.js
+++ test.js
@@ -1,8 +1,14 @@
 // Select the button and greeting paragraph
 const greetButton = document.getElementById('greetButton');
 const greeting = document.getElementById('greeting');
+const fname = document.getElementbyId('fname');
+const lname = document.getElementbyId('lname');
 
 // Add an event listener to the button
 greetButton.addEventListener('click', function() {
     greeting.textContent = "Hello, welcome to my interactive website!";
+});
+
+tombolz.addEventListener('click', function() {
+    greeting.textContent = "Hello nigga";
 });
